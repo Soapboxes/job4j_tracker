5e01e9fed3afffbbdd674912a52fc2008ecf83e1
Soapboxes <furmaks@gmail.com>
2023-12-03 19:49:25 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        switch (position) {
+            case 1 -> System.out.println("Пусть бегут неуклюже");
+            case 2 -> System.out.println("Спокойной ночи");
+            default -> System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox songPosition = new Jukebox();
+        songPosition.music(1);
+        songPosition.music(2);
+        songPosition.music(3);
+
+        for (int i = 0; i <= 2; i++) {
+            System.out.println();
+            songPosition.music(i);
+        }
+    }
+}
