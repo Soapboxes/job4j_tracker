12b26f2689f17189d872024885113f82528b348b
Soapboxes <furmaks@gmail.com>
2023-12-04 22:52:23 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,8 +2,7 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        String dontKnow = "Неизвестное слово: ";
-        return dontKnow + eng;
+        return "Неизвестное слово: " + eng;
     }
 
     public static void main(String[] args) {
