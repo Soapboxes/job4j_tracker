711acafef2365a050a9312b3b45b567ab147df38
Soapboxes <furmaks@gmail.com>
2023-12-05 13:44:05 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,21 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+
+    private String food;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " съел " + this.food);
+    }
 
     public String sound() {
         String voice = "may - may";
@@ -8,9 +23,15 @@ public class Cat {
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
+        Cat gav = new Cat();
+        Cat black = new Cat();
+        String say = gav.sound();
+        gav.eat("fish");
+        gav.giveNick("Гав");
+        gav.show();
+        black.giveNick("черный");
+        black.eat("корм");
+        black.show();
         System.out.println("Peppy says " + say);
     }
 }
