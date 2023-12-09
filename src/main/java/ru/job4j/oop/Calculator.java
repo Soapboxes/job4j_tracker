27299f1efa67caf01e2d0fcb5b5e69c072d0c127
Soapboxes <furmaks@gmail.com>
2023-12-09 15:18:01 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,41 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int numMinus) {
+        return numMinus - x;
+    }
+
+    public int divide(int numDiv) {
+        return numDiv / x;
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public int sumAllOperation(int sum, int minus, int multiply, int divide) {
+        return sum + minus + divide + multiply;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = Calculator.sum(10);
+        System.out.println(result);
+        int rsl = calculator.multiply(5);
+        System.out.println(rsl);
+        int resMin = Calculator.minus(1);
+        System.out.println(resMin);
+        int rslDivide = calculator.divide(2);
+        System.out.println(rslDivide);
+        int allSum = calculator.sumAllOperation(5, 2, 1, 4);
+        System.out.println(allSum);
+
+    }
+}
