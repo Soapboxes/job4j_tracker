debfa489b0188d5aaecd982c9299de5d013821c7
Soapboxes <furmaks@gmail.com>
2024-01-05 18:10:52 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/pojo/ProductContains.java
+++ src/main/java/ru/job4j/pojo/ProductContains.java
@@ -4,7 +4,7 @@ public class ProductContains {
     public static void main(String[] args) {
         Product first = new Product("Milk", 100);
         Product second = new Product("Milk", 100);
-        boolean isEqual = first == second;
+        boolean isEqual = first.equals(second);
         System.out.println(isEqual);
     }
 }
