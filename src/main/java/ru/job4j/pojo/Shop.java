e30b62342b94f372b877395b38b2bdd514f300c4
Soapboxes <furmaks@gmail.com>
2024-01-05 17:26:06 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -17,7 +17,6 @@ public class Shop {
 
     public static int indexOfNull(Product[] products) {
         for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
             if (products[i] == null) {
                 return i;
             }
