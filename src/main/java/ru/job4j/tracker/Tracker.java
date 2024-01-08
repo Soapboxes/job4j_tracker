211d5f8d0c3be0991878781107d25724859a7117
Soapboxes <furmaks@gmail.com>
2024-01-08 19:49:03 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,6 +7,17 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    private int indexOf(int id) {
+        int result = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
@@ -14,22 +25,15 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
 
-     public Item[] findByName(String key) {
+    public Item[] findByName(String key) {
         Item[] findName = new Item[items.length];
         int count = 0;
         for (int index = 0; index < size; index++) {
@@ -39,5 +43,15 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(findName, count);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
     }
 }
