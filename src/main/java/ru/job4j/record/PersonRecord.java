bf98735c6f37987bbaff2ffdd46ec59b9ac56c0b
Soapboxes <furmaks@gmail.com>
2024-02-09 17:52:12 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/record/PersonRecord.java
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -3,10 +3,9 @@ package ru.job4j.record;
 public record PersonRecord(String name, int age) {
     @Override
     public String toString() {
-        return "PersonRecord{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+        return "PersonRecord{"
+                + "name='" + name + '\''
+                + ", age=" + age + '}';
     }
 }
 
