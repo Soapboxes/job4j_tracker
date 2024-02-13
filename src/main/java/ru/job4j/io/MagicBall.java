f0b8282f3e9be43357edd550bfe737bd8a1fc11f
Soapboxes <furmaks@gmail.com>
2024-02-13 21:06:37 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ваш вопрос?");
+        String userAnswer = input.nextLine();
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case 0:
+                System.out.println("Да");
+                break;
+            case 1:
+                System.out.println("Нет");
+                break;
+            default:
+                System.out.println("не знаю");
+                break;
+        }
+    }
+}
+
