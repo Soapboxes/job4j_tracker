57dba058ac5a6c39fc574d7232db2529feb2e0ff
Soapboxes <furmaks@gmail.com>
2024-02-20 15:36:54 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/cast/MainVehicle.java
+++ src/main/java/ru/job4j/cast/MainVehicle.java
@@ -1,15 +1,6 @@
 package ru.job4j.cast;
 
-public class MainVehicle implements Vehicle {
-
-    @Override
-    public void move() {
-    }
-
-    @Override
-    public void passenger() {
-    }
-
+public class MainVehicle {
     public static void main(String[] args) {
         Vehicle train = new Train();
         Vehicle airplane = new Airplane();
