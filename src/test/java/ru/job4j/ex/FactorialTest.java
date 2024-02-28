26759310546ddb5a9d4771ca4bf2743d817f570a
Soapboxes <furmaks@gmail.com>
2024-02-28 18:23:12 +0300

    Репозиторий под проект Tracker

---
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactorialTest {
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                     new Factorial().calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
+    }
+
+    @Test
+    void whenCalculateFactorialForOneThenOne() {
+        int expected = 1;
+        int number = 1;
+        int out = new Factorial().calc(number);
+        assertThat(out).isEqualTo(expected);
+    }
+}
