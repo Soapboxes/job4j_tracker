df54f5095ba9004c5741a9f9c15cf853b04d5e30
Soapboxes <furmaks@gmail.com>
2024-03-02 15:44:12 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,9 +3,8 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
-        for (int index = 0; index < value.length; index++) {
-            if (value[index].equals(key)) {
-                result = index;
+        for (String val : value) {
+            if (key.equals(val)) {
                 return result;
             }
         }
