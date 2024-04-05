8e6903b4483cdac07972102a4c96be1fe51ba9f0
Soapboxes <furmaks@gmail.com>
2024-04-05 10:25:09 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -24,7 +24,7 @@ public final class Airbus extends Aircraft {
 
     public void printCountEngine() {
 
-        int countModel = (name.equals("A380") ? 4 : COUNT_ENGINE);
+        int countModel = ("A380".equals(name) ? 4 : COUNT_ENGINE);
 
         System.out.println("Количество двигателей равно: " + countModel);
     }
