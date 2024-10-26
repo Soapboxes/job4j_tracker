18f51a6c7a9cac5b71a45889ebf896b7cd2fa1c7
Soapboxes <furmaks@gmail.com>
2024-10-26 18:18:19 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -11,12 +11,12 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        for (int i = 0; i < persons.size(); i++) {
-            if (persons.get(i).getName().contains(key)
-                    || persons.get(i).getPhone().contains(key)
-                    || persons.get(i).getSurname().contains(key)
-                    || persons.get(i).getAddress().contains(key)) {
-                result.add(persons.get(i));
+        for (Person person : persons) {
+            if (person.getName().contains(key)
+                    || person.getPhone().contains(key)
+                    || person.getSurname().contains(key)
+                    || person.getAddress().contains(key)) {
+                result.add(person);
             }
         }
         return result;
