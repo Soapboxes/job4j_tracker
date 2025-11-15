8ca43ab354663e27930a361ef9998093604aaefb
Soapboxes <furmaks@gmail.com>
2025-11-15 23:01:53 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/queue/PriorityQueueRunner.java
+++ src/main/java/ru/job4j/queue/PriorityQueueRunner.java
@@ -4,7 +4,18 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueRunner {
     public static void main(String[] args) {
-
+        PriorityQueue<String> stringQueue = new PriorityQueue<>();
+        stringQueue.offer("f");
+        stringQueue.offer("a");
+        stringQueue.offer("s");
+        stringQueue.offer("e");
+        printStringElements(stringQueue);
+        PriorityQueue<Transaction> transactions = new PriorityQueue<>(new TransactionSortDescByAmount());
+        transactions.offer(new Transaction(1, "number_1", 100));
+        transactions.offer(new Transaction(2, "number_2", 1000));
+        transactions.offer(new Transaction(3, "number_3", 85));
+        transactions.offer(new Transaction(4, "number_4", 550));
+        printTransactionElements(transactions);
     }
 
     private static void printStringElements(PriorityQueue<String> queue) {
