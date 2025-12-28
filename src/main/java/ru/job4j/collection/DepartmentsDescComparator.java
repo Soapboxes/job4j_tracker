a1de03eb6a853f6de161100392ba86465db80521
Soapboxes <furmaks@gmail.com>
2025-12-28 12:23:31 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -8,14 +8,11 @@ public class DepartmentsDescComparator implements Comparator<String> {
         String[] leftParts = left.split("/");
         String[] rightParts = right.split("/");
         int firstComparison = rightParts[0].compareTo(leftParts[0]);
-
         if (firstComparison != 0) {
             return firstComparison;
         }
-
         String leftRest = left.substring(leftParts[0].length());
         String rightRest = right.substring(rightParts[0].length());
-
         return leftRest.compareTo(rightRest);
     }
 }
