6772bd4254a8c636921ecc07c95f4f3944a000ee
Soapboxes <furmaks@gmail.com>
2026-01-09 21:56:28 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/function/StrategyUsage.java
+++ src/main/java/ru/job4j/function/StrategyUsage.java
@@ -24,7 +24,6 @@ public class StrategyUsage {
         return function.apply(string);
     }
 
-
     public static void main(String[] args) {
         StrategyUsage usage = new StrategyUsage();
         System.out.println(
